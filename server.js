eab9911b52d6da99afa801df28ec30abdceb01f4
indrianii <agnesia.indriani@gmail.com>
2021-05-19 11:46:18 +0700

    setting routes(seharusnya routes tapi salah ketik jadi router) di server js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 //parser application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//panggil router
+var router = require('./router');
+router(app);
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
